3888a7c7360478e8cadc613fc0069c9590f999e7
Sam Mulube <sam@umbrellium.co.uk>
2019-03-31 22:53:29 +0100

    Add httpoison dependency

--- app/mix.exs
+++ app/mix.exs
@@ -38,7 +38,8 @@ defmodule Decode.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:httpoison, "~> 1.4"}
     ]
   end
 end
