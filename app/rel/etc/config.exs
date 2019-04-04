29b454037719c1a0d258320930ea1d6db06fdd7e
Sam Mulube <sam@umbrellium.co.uk>
2019-04-04 21:44:11 +0100

    Add missing config to get host and port

--- app/rel/etc/config.exs
+++ app/rel/etc/config.exs
@@ -1,3 +1,6 @@
 use Mix.Config
 
-config :demo, DecodeWeb.Endpoint, secret_key_base: System.get_env("SECRET_KEY_BASE")
+config :decode, DecodeWeb.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  url: [host: System.get_env("HOST"), port: {:system, "PORT"}],
+  http: [:inet6, port: System.get_env("PORT") || 4000]
