9f2af522b86ed793da852be86789939457fa4cb5
Sam Mulube <sam@umbrellium.co.uk>
2019-04-24 11:32:25 +0100

    Try adding force SSL config to production

--- app/config/prod.exs
+++ app/config/prod.exs
@@ -15,7 +15,8 @@ config :decode, DecodeWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: ".",
-  version: Application.spec(:decode, :vsn)
+  version: Application.spec(:decode, :vsn),
+  force_ssl: [hsts: true]
 
 # Do not print debug messages in production
 config :logger, level: :info
