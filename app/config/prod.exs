105ce42d0da4f45238987c4fd412edbb4a249243
Sam Mulube <sam@umbrellium.co.uk>
2019-04-24 12:40:24 +0100

    Try different force_ssl config

--- app/config/prod.exs
+++ app/config/prod.exs
@@ -15,7 +15,8 @@ config :decode, DecodeWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: ".",
-  version: Application.spec(:decode, :vsn)
+  version: Application.spec(:decode, :vsn),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
